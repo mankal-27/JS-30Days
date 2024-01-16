d94f1232df629e47c87f240ab7fcef4717319e8d
mankal-27 <mnkalkutagi@gmail.com>
2024-01-16 09:55:58 +0530

    DAY1 Exe

---
+++ DAY1/datatypes.js
@@ -0,0 +1,37 @@
+//use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let variableDataType = require('./variable');
+console.log(typeof variableDataType.stringValue);
+console.log(typeof variableDataType.boolenValue);
+console.log(typeof variableDataType.undefinedValue);
+console.log(typeof variableDataType.nullValue);
+
+//Declare four variables without assigning values
+
+let var1 
+let var2
+let var3
+let var4
+
+//Declare four variables with assigned values
+
+let var5 = 'Manjunatha';
+let var6 = true;
+let var7 = undefined;
+let var8 = null;
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstNameMul = 'Manjunath';
+let lastNameMul = 'Kalkutagi';
+let countryMul = 'India';
+const ageMul = 30;
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName = 'Manjunath', lastName = 'Kalkutagi', country = 'India', age = 30;
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 23;
+let yourAge = 30;
+
+console.log()
---
+++ DAY1/variable.js
@@ -0,0 +1,13 @@
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let stringValue = 'Manjunath';
+let boolenValue = true;
+let undefinedValue = undefined;
+let nullValue = null;
+
+module.exports = {
+    stringValue: stringValue,
+    boolenValue: boolenValue,
+    undefinedValue: undefinedValue,
+    nullValue: nullValue
+}
