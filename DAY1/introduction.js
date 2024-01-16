9685f596bea248c951aa7ab7906a30b60baecf72
mankal-27 <mnkalkutagi@gmail.com>
2024-01-16 09:57:15 +0530

    Rearragne

---
+++ DAY1/introduction.js
@@ -0,0 +1,7 @@
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 23;
+let yourAge = 30;
+
+console.log(`I am ${myAge} old`);
+console.log(`You are ${yourAge} old`);
