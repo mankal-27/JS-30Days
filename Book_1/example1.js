7ecb7bf457392ca21bccc8253441f3e4cd872522
mankal-27 <mnkalkutagi@gmail.com>
2024-01-24 19:33:03 +0530

    function call

--- Book_1/example1.js
+++ Book_1/example1.js
@@ -26,3 +26,15 @@ printAmount(amount * 2);
 
 amount = formatAmount();
 console.log(amount);
+
+const TAX_RATE = 0.08;
+
+function calculateFinalPurchaseAmount(amt){
+    amt = amt + (amt * TAX_RATE);
+    return amt;
+}
+
+var amount = 99.99;
+
+amount = calculateFinalPurchaseAmount(amount);
+console.log(amount.toFixed(2));
