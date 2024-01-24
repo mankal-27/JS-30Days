bb87538f003c2c105a9e6d97382bdde344d9c64b
mankal-27 <mnkalkutagi@gmail.com>
2024-01-24 19:30:00 +0530

    functions

--- Book_1/example1.js
+++ Book_1/example1.js
@@ -2,7 +2,7 @@ var i = 0;
 
 while(true){
     if(i <= 9){
-        console.log(i);
+        //console.log(i);
         i = i + 1;
     }else{
         break;
@@ -10,5 +10,19 @@ while(true){
 }
 
 for(i = 0 ; i<= 9; i++){
-    console.log(i);
+    //console.log(i);
 }
+
+function printAmount(amt){
+    console.log(amt.toFixed(2));
+}
+
+function formatAmount(){
+    return "$" + amount.toFixed(2);
+}
+var amount = 99.99;
+
+printAmount(amount * 2);
+
+amount = formatAmount();
+console.log(amount);
