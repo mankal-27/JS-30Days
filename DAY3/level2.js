8c51d0f7344117870db6e3fb3ef4a4fab873decd
mankal-27 <mnkalkutagi@gmail.com>
2024-01-24 19:14:22 +0530

    updated

--- DAY3/level2.js
+++ DAY3/level2.js
@@ -1,7 +1,10 @@
-// this is your main.js script
+const readline = require('readline');
 //Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
 
 let base = prompt("Enter Base: ")
 let height = prompt("Enter Height: ")
 let area = 0.5 * base * height;
 console.log("Area of Triangle is: " + area);
+
+
+//Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
